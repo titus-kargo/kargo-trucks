959d47b199272be64d36b911a0538c14448796a1
Titus Wahyu Urian <titus.urian@kargo.tech>
2022-05-30 16:58:01 +0700

    Finish all query

---
+++ graph/shipments.resolvers.go
@@ -0,0 +1,39 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/titus-kargo/kargo-trucks/graph/model"
+)
+
+func (r *mutationResolver) SaveShipment(ctx context.Context, id *string, name string, origin string, destination string, deliveryDate string, truckID string) (*model.Shipment, error) {
+	truck := &model.Truck{}
+	for _, v := range r.Trucks {
+		if v.ID == truckID {
+			fmt.Println("save shipment")
+			fmt.Println(v)
+			fmt.Println(id)
+			truck.ID = v.ID
+			truck.PlateNo = v.PlateNo
+		}
+	}
+	shipment := &model.Shipment{
+		ID:           fmt.Sprintf("SHIPMENT-%d", len(r.Shipments)+1),
+		Name:         name,
+		Origin:       origin,
+		Destination:  destination,
+		DeliveryDate: deliveryDate,
+		Truck:        truck,
+	}
+
+	r.Shipments = append(r.Shipments, shipment)
+	return shipment, nil
+}
+
+func (r *queryResolver) PaginatedShipments(ctx context.Context, id *string, name *string, origin *string, destination *string, deliveryDate *string, page int, first int) ([]*model.Shipment, error) {
+	return r.Shipments, nil
+}
---
+++ graph/trucks.resolvers.go
@@ -0,0 +1,35 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/titus-kargo/kargo-trucks/graph/generated"
+	"github.com/titus-kargo/kargo-trucks/graph/model"
+)
+
+func (r *mutationResolver) SaveTruck(ctx context.Context, id *string, plateNo string) (*model.Truck, error) {
+	truck := &model.Truck{
+		ID:      fmt.Sprintf("TRUCK-%d", len(r.Trucks)+1),
+		PlateNo: plateNo,
+	}
+
+	r.Trucks = append(r.Trucks, truck)
+	return truck, nil
+}
+
+func (r *queryResolver) PaginatedTrucks(ctx context.Context, id *string, plateNo *string, page int, first int) ([]*model.Truck, error) {
+	return r.Trucks, nil
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
