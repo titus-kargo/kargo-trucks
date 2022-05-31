a960235150ec6d941681f9dc840a3ab0d49783e2
Titus Wahyu Urian <titus.urian@kargo.tech>
2022-05-31 11:34:46 +0700

    Mulai bikin csv

--- server.go
+++ server.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
 	"net/http"
-	"net/smtp"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
@@ -27,38 +27,41 @@ func main() {
 
 	resolver := &graph.Resolver{}
 	resolver.Init()
+	if len(resolver.Trucks) >= 10 {
+		fmt.Println("Siap untuk mengirim email")
+	}
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: resolver}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
 
-	// Sender data.
-	from := os.Getenv("username")
-	password := os.Getenv("password")
+	// // Sender data.
+	// from := os.Getenv("username")
+	// password := os.Getenv("password")
 
-	// Receiver email address.
-	to := []string{
-		os.Getenv("receiver1"),
-	}
+	// // Receiver email address.
+	// to := []string{
+	// 	os.Getenv("receiver1"),
+	// }
 
-	// SMTP server configuration.
-	smtpHost := "smtp.gmail.com"
-	smtpPort := "587"
+	// // SMTP server configuration.
+	// smtpHost := "smtp.gmail.com"
+	// smtpPort := "587"
 
-	// Message.
-	message := []byte("this is a test email message.")
+	// // Message.
+	// message := []byte("this is a test email message.")
 
-	// Authentication
-	auth := smtp.PlainAuth("", from, password, smtpHost)
+	// // Authentication
+	// auth := smtp.PlainAuth("", from, password, smtpHost)
 
-	// Sending Email.
-	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	// // Sending Email.
+	// err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
 
-	fmt.Println("Email Sent Successfully!")
+	// fmt.Println("Email Sent Successfully!")
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
@@ -70,4 +73,25 @@ func initEnv() error {
 	}
 
 	return nil
+}
+
+func records() {
+	records := [][]string{
+		{"first_name", "last_name", "occupation"},
+		{"John", "Doe", "gardener"},
+		{"Lucy", "Smith", "teacher"},
+		{"Brian", "Bethamy", "programmer"},
+	}
+	f, err := os.Create("users.csv")
+	defer f.Close()
+	if err != nil {
+		log.Fatalln("failed to open file", err)
+	}
+	w := csv.NewWriter(f)
+	defer w.Flush()
+	for _, record := range records {
+		if err := w.Write(record); err != nil {
+			log.Fatalln("error writing record to file", err)
+		}
+	}
 }
